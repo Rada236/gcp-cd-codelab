79c6a58bd2484cfae0be35dbfdbe2d1f72501059
Rada236 <vasatkora@gmail.com>
2018-03-01 11:04:53 +0000

    New html1

--- main.go
+++ main.go
@@ -10,23 +10,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Create Local logger
-	localLogger := log.NewLogfmtLogger(os.Stderr)
-	serviceComponent := os.Getenv("COMPONENT")
-	backendURL := os.Getenv("BACKEND_URL")
-
-	var common CommonService
-	common = commonService{backendURL: backendURL}
-
-	createCommonEndpoints(common)
-	if serviceComponent == "frontend" {
-		createFrontendEndpoints(common)
-	} else if serviceComponent == "backend" {
-		createBackendEndpoints(common)
-	} else {
-		panic("Unknown component: " + serviceComponent)
-	}
-
-	localLogger.Log("msg", "HTTP", "addr", ":8080")
-	localLogger.Log("err", http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":80", nil)
 }
